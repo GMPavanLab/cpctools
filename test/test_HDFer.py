af96b5d205559dcb05b4d5fe6ff5f1dd4f3a09ac
Daniele <iximiel@gmail.com>
2021-12-02 16:17:33 +0100

    completed test for MDA2HDF5

--- test/test_HDFer.py
+++ test/test_HDFer.py
@@ -1,7 +1,9 @@
+from MDAnalysis import coordinates
 import HDF5er
+import h5py
 import pytest
 import MDAnalysis
-from MDAnalysis.topology.MinimalParser import MinimalParser
+from MDAnalysis.topology.MinimalParser import Topology
 from MDAnalysis.coordinates.memory import MemoryReader
 import numpy
 
@@ -11,20 +13,34 @@ import numpy
 def giveUniverse() -> MDAnalysis.Universe:
     traj = numpy.array(
         [
-            [[0, 0, 0], [1, 1, 1], [2, 2, 2]],
-            [[0.1, 0.1, 0.1], [1.1, 1.1, 1.1], [2.1, 2.1, 2.1]],
-            [[0.2, 0.2, 0.2], [1.2, 1.2, 1.2], [2.2, 2.2, 2.2]],
+            [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0], [3.0, 3.0, 3.0]],
+            [[0.1, 0.1, 0.1], [1.1, 1.1, 1.1], [2.1, 2.1, 2.1], [3.1, 3.1, 3.1]],
+            [[0.2, 0.2, 0.2], [1.2, 1.2, 1.2], [2.2, 2.2, 2.2], [3.2, 3.2, 3.2]],
+            [[0.3, 0.3, 0.3], [1.3, 1.3, 1.3], [2.3, 2.3, 2.3], [3.3, 3.3, 3.3]],
+            [[0.4, 0.4, 0.4], [1.4, 1.4, 1.4], [2.4, 2.4, 2.4], [3.4, 3.4, 3.4]],
         ]
     )
-    p = MinimalParser(filename="")  # , n_atoms=3)
-    p.parse(n_atoms=3)
-    u = MDAnalysis.Universe(p, traj, format=MemoryReader, order="fac")
-    u.trajectory
-    for frame in u.trajectory:
-        u.atoms.positions()
+    u = MDAnalysis.Universe.empty(4, trajectory=True)
+    u.add_TopologyAttr("type", ["H", "H", "H", "H"])
+    u.trajectory.set_array(traj, "fac")
     return u
 
 
 def test_MDA2HDF5():
-    # Given an MDA Universe:
-    u = giveUniverse()
+    # Given an MDA Universe :
+    fourAtomsFiveFrames = giveUniverse()
+    HDF5er.MDA2HDF5(fourAtomsFiveFrames, "test.hdf5", "4Atoms5Frames")
+    with h5py.File("test.hdf5", "r") as hdf5test:
+        # this checks also that the group has been created
+        group = hdf5test["Trajectories/4Atoms5Frames"]
+        # this checks also that the dataset has been created
+        nat = len(group["Types"])
+        for i, f in enumerate(fourAtomsFiveFrames.trajectory):
+            assert nat == len(fourAtomsFiveFrames.atoms)
+            for atomID in range(nat):
+                for coord in range(3):
+                    assert (
+                        group["Trajectory"][i, atomID, coord]
+                        - fourAtomsFiveFrames.atoms.positions[atomID, coord]
+                        < 1e-8
+                    )
