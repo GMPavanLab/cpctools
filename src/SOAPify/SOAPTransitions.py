adf4ddeaa5924a759de6987db10ea4d0e644c891
Daniele <iximiel@gmail.com>
2022-03-10 13:43:07 +0100

    small reorganization of the code

---
+++ src/SOAPify/SOAPTransitions.py
@@ -0,0 +1,72 @@
+from .SOAPClassify import SOAPclassification
+import numpy as np
+
+
+def transitionMatrixFromSOAPClassification(
+    data: SOAPclassification, stride: int = 1
+) -> "np.ndarray[float]":
+    """Generates the unnormalized matrix of the transitions from a :func:`classifyWithSOAP`
+
+        The matrix is organized in the following way:
+        for each atom in each frame we increment by one the cell whose row is the
+        state at the frame `n-stride` and the column is the state at the frame `n`
+
+    Args:
+        data (SOAPclassification): the results of the soapClassification from :func:`classifyWithSOAP`
+        stride (int): the stride in frames between each state confrontation. Defaults to 1.
+        of the groups that contain the references in hdf5FileReference
+    Returns:
+        np.ndarray[float]: the unnormalized matrix of the transitions
+    """
+    nframes = len(data.references)
+    nat = len(data.references[0])
+    # +1 in case of errors
+    nclasses = len(data.legend) + 1
+    transMat = np.zeros((nclasses, nclasses), np.dtype(float))
+
+    for frameID in range(stride, nframes, 1):
+        for atomID in range(0, nat):
+            classFrom = data.references[frameID - stride][atomID]
+            classTo = data.references[frameID][atomID]
+            transMat[classFrom, classTo] += 1
+    return transMat
+
+
+def normalizeMatrix(transMat: "np.ndarray[float]") -> "np.ndarray[float]":
+    """normalizes a matrix that is an ouput of :func:`transitionMatrixFromSOAPClassification`
+
+    The matrix is normalized with the criterion that the sum of each **row** is `1`
+
+    Args:
+        np.ndarray[float]: the unnormalized matrix of the transitions
+
+    Returns:
+        np.ndarray[float]: the normalized matrix of the transitions
+    """
+    for row in range(transMat.shape[0]):
+        sum = np.sum(transMat[row, :])
+        if sum != 0:
+            transMat[row, :] /= sum
+    return transMat
+
+
+def transitionMatrixFromSOAPClassificationNormalized(
+    data: SOAPclassification, stride: int = 1
+) -> "np.ndarray[float]":
+    """Generates the normalized matrix of the transitions from a :func:`classifyWithSOAP` and normalize it
+
+        The matrix is organized in the following way:
+        for each atom in each frame we increment by one the cell whose row is the
+        state at the frame `n-stride` and the column is the state at the frame `n`
+
+        The matrix is normalized with the criterion that the sum of each **row** is `1`
+
+    Args:
+        data (SOAPclassification): the results of the soapClassification from :func:`classifyWithSOAP`
+        stride (int): the stride in frames between each state confrontation. Defaults to 1.
+        of the groups that contain the references in hdf5FileReference
+    Returns:
+        np.ndarray[float]: the normalized matrix of the transitions
+    """
+    transMat = transitionMatrixFromSOAPClassification(data, stride)
+    return normalizeMatrix(transMat)
--- src/SOAPify/__init__.py
+++ src/SOAPify/__init__.py
@@ -2,3 +2,4 @@ from .SOAPClassify import *
 from .SOAPbase import *
 from .Saponify import *
 from .utils import *
+from .SOAPTransitions import *
