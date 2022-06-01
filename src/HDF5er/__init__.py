20fd014349d143ae3885fa075e4561ebfe1d4425
Daniele <Iximiel@users.noreply.github.com>
2022-06-01 16:46:56 +0200

    Enhancement of Saponify (#20)
    * the SOAP engine is loaded separately
    
    * small qol in saponify, broken interface
    
    * rationalization of Saponify.py

--- src/HDF5er/__init__.py
+++ src/HDF5er/__init__.py
@@ -1,3 +1,4 @@
 from .MDA2HDF5 import *
 from .ase2HDF5 import *
 from .HDF5To import *
+from .HDF5erUtils import isTrajectoryGroup
--- src/SOAPify/Saponify.py
+++ src/SOAPify/Saponify.py
@@ -1,7 +1,7 @@
 import warnings
 import h5py
 from dscribe.descriptors import SOAP
-from HDF5er import HDF52AseAtomsChunckedwithSymbols as HDF2ase
+from HDF5er import HDF52AseAtomsChunckedwithSymbols as HDF2ase, isTrajectoryGroup
 import time
 from typing import Iterable
 
@@ -40,9 +40,9 @@ def saponifyWorker(
             del centersMask.attrs["centersIndexes"]
     else:
         SOAPoutDataset.attrs.create("centersIndexes", centersMask)
-        print(centersMask)
-        print(SOAPoutDataset.attrs["centersIndexes"])
-        print(type(SOAPoutDataset.attrs["centersIndexes"]))
+        # print(centersMask)
+        # print(SOAPoutDataset.attrs["centersIndexes"])
+        # print(type(SOAPoutDataset.attrs["centersIndexes"]))
 
     nspecies = len(soapEngine.species)
     for i in range(nspecies):
@@ -82,6 +82,69 @@ def saponifyWorker(
             print(f"delta create= {t2-t1}")
 
 
+def getSoapEngine(
+    species: "list[str]",
+    SOAPrcut: float,
+    SOAPnmax: int,
+    SOAPlmax: int,
+    SOAP_respectPBC: bool = True,
+    SOAPkwargs: dict = {},
+) -> SOAP:
+    """Returns a soap engine already set up
+
+    Returns:
+        SOAP: the soap engine already set up
+    """
+    SOAPkwargs.update(
+        dict(
+            species=species,
+            periodic=SOAP_respectPBC,
+            rcut=SOAPrcut,
+            nmax=SOAPnmax,
+            lmax=SOAPlmax,
+        )
+    )
+    if "sparse" in SOAPkwargs.keys():
+        if SOAPkwargs["sparse"]:
+            SOAPkwargs["sparse"] = False
+            warnings.warn("sparse output is not supported yet, switching to dense")
+    return SOAP(**SOAPkwargs)
+
+
+def applySOAP(
+    trajContainer: h5py.Group,
+    SOAPoutContainer: h5py.Group,
+    key: str,
+    soapEngine: SOAP,
+    centersMask: "list|None" = None,
+    SOAPOutputChunkDim: int = 100,
+    SOAPnJobs: int = 1,
+):
+    NofFeatures = soapEngine.get_number_of_features()
+    symbols = trajContainer["Types"].asstr()[:]
+    nCenters = len(symbols) if centersMask is None else len(centersMask)
+
+    if key not in SOAPoutContainer.keys():
+        SOAPoutContainer.create_dataset(
+            key,
+            (0, nCenters, NofFeatures),
+            compression="gzip",
+            compression_opts=9,
+            chunks=(SOAPOutputChunkDim, nCenters, NofFeatures),
+            maxshape=(None, nCenters, NofFeatures),
+        )
+    SOAPout = SOAPoutContainer[key]
+    SOAPout.resize((len(trajContainer["Trajectory"]), nCenters, NofFeatures))
+    saponifyWorker(
+        trajContainer,
+        SOAPout,
+        soapEngine,
+        centersMask,
+        SOAPOutputChunkDim,
+        SOAPnJobs,
+    )
+
+
 def saponifyGroup(
     trajContainers: "h5py.Group|h5py.File",
     SOAPoutContainers: "h5py.Group|h5py.File",
@@ -119,11 +182,7 @@ def saponifyGroup(
     """
     soapEngine = None
     for key in trajContainers.keys():
-        if (
-            "Trajectory" in trajContainers[key].keys()
-            and "Types" in trajContainers[key].keys()
-            and "Box" in trajContainers[key].keys()
-        ):
+        if isTrajectoryGroup(trajContainers[key]):
             traj = trajContainers[key]
             symbols = traj["Types"].asstr()[:]
             # TODO: unify the soap initialization with saponify
@@ -135,55 +194,36 @@ def saponifyGroup(
                 centersMask = [
                     i for i in range(len(symbols)) if symbols[i] in SOAPatomMask
                 ]
-            nCenters = len(symbols) if centersMask is None else len(centersMask)
-
             if soapEngine is None:
-                species = list(set(symbols))
-                SOAPkwargs.update(
-                    dict(
-                        species=species,
-                        periodic=SOAP_respectPBC,
-                        rcut=SOAPrcut,
-                        nmax=SOAPnmax,
-                        lmax=SOAPlmax,
-                    )
+                soapEngine = getSoapEngine(
+                    species=list(set(symbols)),
+                    SOAPrcut=SOAPrcut,
+                    SOAPnmax=SOAPnmax,
+                    SOAPlmax=SOAPlmax,
+                    SOAP_respectPBC=SOAP_respectPBC,
+                    SOAPkwargs=SOAPkwargs,
                 )
-                if "sparse" in SOAPkwargs.keys():
-                    if SOAPkwargs["sparse"]:
-                        SOAPkwargs["sparse"] = False
-                        warnings.warn(
-                            "sparse output is not supported yet, switching to dense"
-                        )
-                soapEngine = SOAP(**SOAPkwargs)
-                NofFeatures = soapEngine.get_number_of_features()
-
-            if key not in SOAPoutContainers.keys():
-                SOAPoutContainers.create_dataset(
-                    key,
-                    (0, nCenters, NofFeatures),
-                    compression="gzip",
-                    compression_opts=9,
-                    chunks=(100, nCenters, NofFeatures),
-                    maxshape=(None, nCenters, NofFeatures),
-                )
-            SOAPout = SOAPoutContainers[key]
-            SOAPout.resize((len(traj["Trajectory"]), nCenters, NofFeatures))
-            saponifyWorker(
-                traj, SOAPout, soapEngine, centersMask, SOAPOutputChunkDim, SOAPnJobs
+            applySOAP(
+                traj,
+                SOAPoutContainers,
+                key,
+                soapEngine,
+                centersMask,
+                SOAPOutputChunkDim,
+                SOAPnJobs,
             )
 
 
 def saponify(
-    trajFname: str,
-    trajectoryGroupPath: str,
-    outputFname: str,
-    exportDatasetName: str,
+    trajContainer: "h5py.Group|h5py.File",
+    SOAPoutContainer: "h5py.Group|h5py.File",
     SOAPrcut: float,
     SOAPnmax: int,
     SOAPlmax: int,
     SOAPOutputChunkDim: int = 100,
     SOAPnJobs: int = 1,
     SOAPatomMask: str = None,
+    centersMask: Iterable = None,  # TODO: document this
     SOAP_respectPBC: bool = True,
     SOAPkwargs: dict = {},
 ):
@@ -214,93 +254,46 @@ def saponify(
         considered to be periodic (option passed to dscribe's SOAP). Defaults to True.
         SOAPkwargs (dict, optional): additional keyword arguments to be passed to the SOAP engine. Defaults to {}.
     """
-
-    with h5py.File(trajFname, "r") as trajLoader, h5py.File(
-        outputFname, "a"
-    ) as soapOffloader:
-        traj = trajLoader[f"Trajectories/{trajectoryGroupPath}"]
-
-        symbols = traj["Types"].asstr()[:]
+    if isTrajectoryGroup(trajContainer):
+        symbols = trajContainer["Types"].asstr()[:]
         if SOAPatomMask is not None and centersMask is not None:
             raise Exception(
                 f"saponify: You can't use both SOAPatomMask and centersMask"
             )
         if SOAPatomMask is not None:
             centersMask = [i for i in range(len(symbols)) if symbols[i] in SOAPatomMask]
-
-        species = list(set(symbols))
-        SOAPkwargs.update(
-            dict(
-                species=species,
-                periodic=SOAP_respectPBC,
-                rcut=SOAPrcut,
-                nmax=SOAPnmax,
-                lmax=SOAPlmax,
-            )
+        soapEngine = getSoapEngine(
+            species=list(set(symbols)),
+            SOAPrcut=SOAPrcut,
+            SOAPnmax=SOAPnmax,
+            SOAPlmax=SOAPlmax,
+            SOAP_respectPBC=SOAP_respectPBC,
+            SOAPkwargs=SOAPkwargs,
         )
-        if "sparse" in SOAPkwargs.keys():
-            if SOAPkwargs["sparse"]:
-                SOAPkwargs["sparse"] = False
-                warnings.warn("sparse output is not supported yet, switching to dense")
-        soapEngine = SOAP(
-            **SOAPkwargs,
+        exportDatasetName = trajContainer.name.split("/")[-1]
+        applySOAP(
+            trajContainer,
+            SOAPoutContainer,
+            exportDatasetName,
+            soapEngine,
+            centersMask,
+            SOAPOutputChunkDim,
+            SOAPnJobs,
         )
-
-        NofFeatures = soapEngine.get_number_of_features()
-
-        soapDir = soapOffloader.require_group("SOAP")
-        nCenters = len(symbols) if centersMask is None else len(centersMask)
-        if exportDatasetName not in soapDir.keys():
-            soapDir.create_dataset(
-                exportDatasetName,
-                (0, nCenters, NofFeatures),
-                compression="gzip",
-                compression_opts=9,
-                chunks=(100, nCenters, NofFeatures),
-                maxshape=(None, nCenters, NofFeatures),
-            )
-        SOAPout = soapDir[exportDatasetName]
-        SOAPout.resize((len(traj["Trajectory"]), nCenters, NofFeatures))
-        saponifyWorker(
-            traj, SOAPout, soapEngine, centersMask, SOAPOutputChunkDim, SOAPnJobs
-        )
-        return
-        for chunkTraj in traj["Trajectory"].iter_chunks():
-            chunkBox = (chunkTraj[0], slice(0, 6, 1))
-            print(f'working on trajectory chunk "{chunkTraj}"')
-            print(f'   and working on box chunk "{repr(chunkBox)}"')
-            # load in memory a chunk of data
-            atoms = HDF2ase(traj, chunkTraj, chunkBox, symbols)
-
-            jobchunk = min(SOAPOutputChunkDim, len(atoms))
-            jobStart = 0
-            jobEnd = jobStart + jobchunk
-            while jobStart < len(atoms):
-                t1 = time.time()
-                frameStart = jobStart + chunkTraj[0].start
-                FrameEnd = jobEnd + chunkTraj[0].start
-                print(f"working on frames: [{frameStart}:{FrameEnd}]")
-                SOAPout[frameStart:FrameEnd] = soapEngine.create(
-                    atoms[jobStart:jobEnd],
-                    positions=[centersMask] * jobchunk,
-                    n_jobs=SOAPnJobs,
-                )
-                t2 = time.time()
-                jobchunk = min(SOAPOutputChunkDim, len(atoms) - jobEnd)
-                jobStart = jobEnd
-                jobEnd = jobStart + jobchunk
-                print(f"delta create= {t2-t1}")
+    else:
+        raise Exception(f"saponify: The input object is not a trajectory group.")
 
 
 if __name__ == "__main__":
     # this is an example script for Applying the SOAP analysis on a trajectory saved on an
     # HDF5 file formatted with our HDF5er and save the result in another HDF5 file
-    saponify(
-        "Water.hdf5",
-        "1ns",
-        "WaterSOAP.hdf5",
-        "1nSOAP",
-        SOAPatomMask="O",
-        SOAPOutputChunkDim=100,
-        SOAPnJobs=12,
-    )
+    with h5py.File("Water.hdf5", "r") as trajLoader, h5py.File(
+        "WaterSOAP.hdf5", "a"
+    ) as soapOffloader:
+        saponify(
+            trajLoader[f"Trajectories/1ns"],
+            soapOffloader.require_group("SOAP"),
+            SOAPatomMask="O",
+            SOAPOutputChunkDim=100,
+            SOAPnJobs=12,
+        )
