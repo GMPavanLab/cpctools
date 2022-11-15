98b5d3783a5172c9a10c4998d557269c9f1daee1
Iximiel <Iximiel@users.noreply.github.com>
2022-11-15 16:12:46 +0000

    deploy: 129e335e5aa0df112328b5172e6a8de0b062e161

--- src/SOAPify/__init__.py
+++
@@ -1,8 +0,0 @@
-from .SOAPClassify import *
-from .SOAPbase import *
-from .Saponify import *
-from .utils import *
-from .SOAPTransitions import *
-from .SOAPengine import *
-
-__version__ = "v0.0.3"
